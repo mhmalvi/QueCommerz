34fd5753934f36918d158ce8c908c0a10e8a494d
Zakaria <jakariablaine120@gmail.com>
2021-09-30 17:10:25 +0600

    Add a maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Site Maintenance</title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+		<style>
+		  html, body { padding: 0; margin: 0; width: 100%; height: 100%; }
+		  * {box-sizing: border-box;}
+		  body
+		  {
+		  	text-align: center; padding: 0; background: #efefef; color: #5f5f5f; font-family: Open Sans;
+		  	font-family: Open Sans; font-weight: 100; font-size: 20px; text-align: center; display: -webkit-box; display: -ms-flexbox; display: flex; -webkit-box-pack: center; -ms-flex-pack: center; justify-content: center; -webkit-box-align: center; -ms-flex-align: center; align-items: center;
+		  }
+		  h1
+		  {
+		  	font-size: 50px; font-weight: 100; text-align: center;
+		  }
+		  article
+		  {
+		  	display: block; width: 700px; padding: 50px; margin: 0 auto;
+		  }
+		  a
+		  {
+		  	color: #fff; font-weight: bold;
+		  }
+		  a:hover
+		  {
+		  	text-decoration: none;
+		  }
+		  svg
+		  {
+		  	width: 75px; margin-top: 1em;
+		  }
+		</style>
+	</head>
+
+	<body>
+		<article>
+		    <img src="/assets/maintenance.svg" width="400px">
+		    <h1>We&rsquo;ll be back soon!</h1>
+		    <div>
+		        <p>Sorry for the inconvenience. We&rsquo;re performing some maintenance at the moment. We&rsquo;ll be back up shortly!</p>
+		        <p>&mdash; The QuadQue Team</p>
+		    </div>
+		</article>
+	</body>
+</html>
